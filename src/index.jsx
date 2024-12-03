c56941ce87e640ea7c2d227409213059935cc02b
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-12-03 15:10:03 +0100

    Set up project with Parcel and React".

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss'; // Import your styles
+
+const App = () => <h1>Good Morning</h1>;
+
+ReactDOM.render(<App />, document.getElementById('root'));
