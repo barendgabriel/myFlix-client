18556141423d9f747881f30b26c44aac250f1bb9
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-12-04 14:29:19 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss'; // Import your styles
+import { MovieList } from './components/MovieList';
 
-const App = () => <h1>Good Morning</h1>;
+const movies = [
+  {
+    title: 'Jurrassic Park',
+    genre: 'Thriller',
+  },
+  {
+    title: 'Godzilla',
+    genre: 'Comedy',
+  },
+  {
+    title: 'Evil Dead',
+    genre: 'Horror',
+  },
+];
+const App = () => {
+  return (
+    <div>
+      <h1>Netflix</h1>
+      <div>
+        <MovieList movies={movies} />
+      </div>
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
