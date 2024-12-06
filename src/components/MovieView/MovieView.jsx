91304bbbc78dd7dbde89ea3ce40162c9b6e258ac
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-12-06 18:46:36 +0100

    Update MovieView.jsx

--- src/components/MovieView/MovieView.jsx
+++ src/components/MovieView/MovieView.jsx
@@ -1,12 +1,21 @@
-// src/components/MovieView/MovieView.jsx
 import React from 'react';
 
-export const MovieView = ({ movie }) => {
+export const MovieView = ({ movie, onBackClick }) => {
   return (
     <div>
-      <h2>{movie.title}</h2>
-      <p>Genre: {movie.genre}</p>
-      <p>Details: Additional movie details go here.</p>
+      <h1>{movie.title}</h1>
+      <img
+        src={`/images/${movie.imagePath}`}
+        alt={movie.title}
+        style={{ width: '300px', height: 'auto' }}
+      />
+      <p>
+        <strong>Genre:</strong> {movie.genre}
+      </p>
+      <p>
+        <strong>Description:</strong> {movie.description}
+      </p>
+      <button onClick={onBackClick}>Back</button>
     </div>
   );
 };
