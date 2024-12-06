eeb9f9b021d00089964e8ad572e42bfaf945a5bf
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-12-06 20:26:53 +0100

    Update MainView.jsx

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -6,8 +6,8 @@ export const MainView = ({ movies }) => {
   return (
     <div>
       <h2>Movie List</h2>
-      {movies.map((movie, index) => (
-        <Link to={`/movie/${movie.title}`} key={index}>
+      {movies.map((movie) => (
+        <Link to={`/movie/${movie.title}`} key={movie.title}>
           <MovieCard movie={movie} />
         </Link>
       ))}
