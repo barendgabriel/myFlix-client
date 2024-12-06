f49229bc03aefe3bc03c391218ed10915e740060
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-12-06 18:52:42 +0100

    Update MovieCard.jsx

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -4,6 +4,12 @@ export const MovieCard = ({ movie }) => {
   return (
     <div>
       <h3>{movie.title}</h3>
+      {/* Display the movie image */}
+      <img
+        src={`/images/${movie.image}`}
+        alt={`${movie.title} Poster`}
+        style={{ width: '150px', height: 'auto' }}
+      />
     </div>
   );
 };
