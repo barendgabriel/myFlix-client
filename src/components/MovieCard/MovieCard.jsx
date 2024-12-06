13862f50f10e13d55d94a2cd35ee9a0706301297
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-12-06 18:46:31 +0100

    Update MovieCard.jsx

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,11 +1,9 @@
-// src/components/MovieCard/MovieCard.jsx
 import React from 'react';
 
 export const MovieCard = ({ movie }) => {
   return (
     <div>
       <h3>{movie.title}</h3>
-      <p>Genre: {movie.genre}</p>
     </div>
   );
 };
