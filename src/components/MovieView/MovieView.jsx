0a9df8394894e16e52e5a43efab262cdbbd96b16
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-12-06 20:27:00 +0100

    Update MovieView.jsx

--- src/components/MovieView/MovieView.jsx
+++ src/components/MovieView/MovieView.jsx
@@ -5,17 +5,19 @@ export const MovieView = ({ movie, onBackClick }) => {
     <div>
       <h1>{movie.title}</h1>
       <img
-        src={`/images/${movie.imagePath}`}
+        src={`/images/${movie.imagePath}`} // Image path from public/images
         alt={movie.title}
-        style={{ width: '300px', height: 'auto' }}
+        style={{ width: '300px', height: 'auto' }} // Styling the image size
       />
       <p>
-        <strong>Genre:</strong> {movie.genre}
+        <strong>Genre:</strong> {movie.genre} {/* Display the genre */}
       </p>
       <p>
-        <strong>Description:</strong> {movie.description}
+        <strong>Description:</strong> {movie.description}{' '}
+        {/* Display the description */}
       </p>
-      <button onClick={onBackClick}>Back</button>
+      <button onClick={onBackClick}>Back</button>{' '}
+      {/* Back button with callback */}
     </div>
   );
 };
