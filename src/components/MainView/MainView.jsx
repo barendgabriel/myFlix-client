813ac233a56deb54001df3e5922996383616014c
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-12-09 13:36:29 +0100

    Update MainView.jsx

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,24 +1,22 @@
-// components/MainView/MainView.jsx
+// src/components/MainView/MainView.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export class MainView extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Movie List</h1>
-        <ul>
-          <li>
-            <Link to="/movies/Evil%20Dead">Evil Dead</Link>
-          </li>
-          <li>
-            <Link to="/movies/Godzilla">Godzilla</Link>
-          </li>
-          <li>
-            <Link to="/movies/Jurassic%20Park">Jurassic Park</Link>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+const MainView = () => (
+  <div>
+    <h1>Movie List</h1>
+    <ul>
+      <li>
+        <Link to="/movies/Evil%20Dead">Evil Dead</Link>
+      </li>
+      <li>
+        <Link to="/movies/Godzilla">Godzilla</Link>
+      </li>
+      <li>
+        <Link to="/movies/Jurassic%20Park">Jurassic Park</Link>
+      </li>
+    </ul>
+  </div>
+);
+
+export default MainView;
