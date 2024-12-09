30e70fbce42fb66324abf6463a4cd3e96b5c83c7
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-12-09 13:36:33 +0100

    Update MovieCard.jsx

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,19 +1,21 @@
 // src/components/MovieCard/MovieCard.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-export const MovieCard = ({ movie }) => {
-  return (
-    <div
-      style={{
-        border: '1px solid #ccc',
-        padding: '10px',
-        margin: '10px',
-        borderRadius: '8px',
-        cursor: 'pointer',
-        maxWidth: '200px',
-      }}
-    >
-      <h3>{movie.title}</h3>
-    </div>
-  );
-};
+const MovieCard = ({ movie }) => (
+  <div
+    style={{
+      border: '1px solid #ccc',
+      padding: '10px',
+      margin: '10px',
+      borderRadius: '8px',
+      cursor: 'pointer',
+      maxWidth: '200px',
+    }}
+  >
+    <h3>{movie.title}</h3>
+    <Link to={`/movies/${encodeURIComponent(movie.title)}`}>View Details</Link>
+  </div>
+);
+
+export default MovieCard;
