3bc7cc905c75e0fb3d34a6355560e060f6821a3a
barendgabriel <jansenvanrensburg.ben@gmail.com>
2024-12-13 16:12:09 +0100

    Setup index.jsx with Routing
    Updated index.jsx to set up React Router for navigation.
    Wrapped the app with BrowserRouter to enable routing capabilities.
    Configured the entry point to work seamlessly with the MainView component.

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,24 @@
-// index.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 import MainView from './components/MainView/MainView'; // Adjusted path
 import MovieView from './components/MovieView/MovieView'; // Adjusted path
+import './index.scss'; // Ensure Bootstrap is imported here
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<MainView />} />
-        <Route path="/movies/:movieTitle" element={<MovieView />} />
-      </Routes>
+      <Container>
+        <Row className="justify-content-center">
+          <Col xs={12} md={10}>
+            <Routes>
+              <Route path="/" element={<MainView />} />
+              <Route path="/movies/:movieTitle" element={<MovieView />} />
+            </Routes>
+          </Col>
+        </Row>
+      </Container>
     </Router>
   );
 };
